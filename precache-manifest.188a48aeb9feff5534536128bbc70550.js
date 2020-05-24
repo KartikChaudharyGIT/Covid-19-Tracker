ae483d44356b096f799388ca8be5491345f2027e
kartikchaudharyGIT <kartikchaudhary89@gmail.com>
2020-05-24 15:14:05 +0530

    Updates

---
+++ precache-manifest.188a48aeb9feff5534536128bbc70550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e3552fff2421e6fcf16290336807f0",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "6b4e1aac3e188cd4c6c7",
+    "url": "/Covid-19-Tracker/static/css/main.ef3a145d.chunk.css"
+  },
+  {
+    "revision": "029140f839c964b8028f",
+    "url": "/Covid-19-Tracker/static/js/2.eb0d61ee.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.eb0d61ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4e1aac3e188cd4c6c7",
+    "url": "/Covid-19-Tracker/static/js/main.97933469.chunk.js"
+  },
+  {
+    "revision": "e00e62938f203eb3d4dd",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.84b01688.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
